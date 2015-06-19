6f8377b7db5c7417660355caad036b9c5e10229b
Gert-Jan van der Wel <hi@gertjanvanderwel.com>
2015-06-19 20:32:55 +0200

    got basic buy and sell api working

--- server.js
+++ server.js
@@ -1,17 +1,69 @@
+var express = require('express');
+var parser = require('body-parser');
 var http = require('http');
 
-var interval = 5000;
-var symbols = [
-	'aapl',
-    'goog',
-    'amzn',
-    'fb',
-    'twtr',
-    'z'
-];
+var teams = {
+    'gertje' : {
+        account : 2000,
+        transactions: []
+    }
+}
 
-function query() {
-    var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+
+var app = express();
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+var router = express.Router();
+
+// buy
+router.route('/buy')
+    .post(function(req, res) {
+        var b = req.body;
+        doTransaction('buy', b.symbol, b.amount, b.team, function(transaction) {
+            res.json(transaction);
+        });
+    });
+
+// sell
+router.route('/sell')
+    .post(function(req, res) {
+        var b = req.body;
+        doTransaction('sell', b.symbol, b.amount, b.team, function(transaction) {
+            res.json(transaction);
+        });
+    });
+
+app.use('/', router);
+app.listen(8080);
+console.log('Started trading game server at port 8080...')
+
+function doTransaction(action, symbol, amount, teamName, callback) {
+    var team = teams[teamName],
+        account = team.account;
+
+    queryYahoo(symbol, function(price) {
+        var total = price * amount;
+
+        if(action === 'buy' && account >= total) {
+            team.account = account - total;
+        }
+
+        if(action === 'sell') {
+            team.account = account + total;
+        }
+
+        transaction = {action:action, symbol:symbol, price:price, amount:amount, account:team.account};
+        team.transactions.push(transaction)
+
+        console.log(transaction);
+        callback(transaction);
+
+    });
+}
+
+function queryYahoo(symbol, callback) {
+    var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20%3D%22"+ symbol +"%22&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
 
     http.get(url, function(res) {
         var body = '';
@@ -22,13 +74,7 @@ function query() {
 
         res.on('end', function() {
             var response = JSON.parse(body);
-            var results = response.query.results.quote;
-
-            for (var i = 0; i < results.length; i++) {
-                console.log(results[i].symbol, results[i].Ask)
-            }
-        })
+            callback(response.query.results.quote.Ask);
+        });
     })
 }
-
-setInterval(query, 1000);
